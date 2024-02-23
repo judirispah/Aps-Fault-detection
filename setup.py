c33876df5d4a40ad6fbd3634c561ebb3ebd248ab
judirispah <judi.rispah.123@gmail.com>
2024-02-23 15:30:01 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sensor",
+    version="0.0.0",
+    author="Judi Rispah",
+    author_email="judi.rispah.123@gmail.com",
+    packages=find_packages()
+)
+
+#It is helpful for considering sensor as local package
+#for runing requirements.txt
